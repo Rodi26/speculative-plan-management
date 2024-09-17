31bb4b48a5805630c7459bb445b1fbf677c7fffa
Rodolphe Fontaine <30529563+Rodi26@users.noreply.github.com>
2024-09-17 17:19:42 +0200

    Update main.tf

--- speculative-plan-management-1/main.tf
+++ speculative-plan-management-1/main.tf
@@ -2,5 +2,7 @@ output "hello_world" {
   value = "Hello World from directory speculative-plan-management-1!"
 }
 
-resource "null_resource" "my_null_resource" {}
+resource "null_resource" "my_null_resource" {
+  count = 1
+}
 
